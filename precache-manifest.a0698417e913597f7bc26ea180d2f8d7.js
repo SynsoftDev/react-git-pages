b06af821fadea28425bcd3a3a012a03435ab2d3c
Ashish Gokhale <ashish.g@synsoftglobal.com>
2020-09-16 18:01:50 +0530

    Updates

---
+++ precache-manifest.a0698417e913597f7bc26ea180d2f8d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8830cfdd4dbf12fd50bc0b134a0c45e",
+    "url": "/react-git-pages/index.html"
+  },
+  {
+    "revision": "665ffac6675c55b174df",
+    "url": "/react-git-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/react-git-pages/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-git-pages/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "665ffac6675c55b174df",
+    "url": "/react-git-pages/static/js/main.d8df3679.chunk.js"
+  },
+  {
+    "revision": "b764b3648240658ba3b2",
+    "url": "/react-git-pages/static/js/runtime-main.63233353.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-git-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
